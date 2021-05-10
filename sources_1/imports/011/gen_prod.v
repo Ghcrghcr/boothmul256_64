9a211ff7a17f4fbb4c24dedde1bdd66a8cc9d2a7
chenchangrong <chenchangrong214@163.com>
2021-05-10 15:14:20 +0800

    change the test_bench

--- sources_1/imports/011/gen_prod.v
+++ sources_1/imports/011/gen_prod.v
@@ -23,9 +23,9 @@ begin
 	if (zero)
 		prod_pre = 0;
 	else if (one)
-		prod_pre = { { (prod_len-a_len){A[a_len-1]} }, A};
+		prod_pre = { { (prod_len-a_len){A[a_len-1]} }, A};		//prod_pre = { { (prod_len-a_len){A[a_len-1]} }, A};
 	else if (two)
-		prod_pre = { { (prod_len-a_len-1){A[a_len-1]} }, A, 1'b0};
+		prod_pre = { { (prod_len-a_len-1){A[a_len-1]} }, A, 1'b0};		//prod_pre = { { (prod_len-a_len-1){A[a_len-1]} }, A, 1'b0};
 end
 
 assign prod = neg ? ( ~prod_pre+1'b1 ) : prod_pre;
